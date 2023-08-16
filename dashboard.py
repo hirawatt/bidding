c9f0a77542c97bd8ddeb13109f3dea37f37bbd1b
VH <hirawatt@pm.me>
2023-08-16 17:45:57 +0530

    added page config + server config

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,22 @@
 import streamlit as st
 import pandas as pd
 
-players_list_upload = st.file_uploader("Upload Players List", type=["csv"])
+# credentials
+page_title = "CricStars - Dashboard"
+
+# streamlit
+st.set_page_config(
+    '{}'.format(page_title),
+    '⛽',
+    layout='wide',
+    initial_sidebar_state='collapsed',
+    menu_items={
+        "Get Help": "https://cricstars.streamlit.app",
+        "About": "CrickStars App",
+    },
+)
+
+players_list_upload = st.sidebar.file_uploader("Upload Players List", type=["csv"])
 players_list = pd.read_csv(players_list_upload)
 st.dataframe(players_list)
 
